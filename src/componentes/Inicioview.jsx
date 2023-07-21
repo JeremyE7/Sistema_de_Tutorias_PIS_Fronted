969ab10db5acda5c0ea770fa8076cb810952d4f1
Jeremy Encalada <encaladajeremy@gmail.com>
2023-07-20 19:04:40 -0500

    Tabla de tutorias pendientes

--- src/componentes/Inicioview.jsx
+++ src/componentes/Inicioview.jsx
@@ -1,9 +1,18 @@
+<<<<<<< HEAD
 import React from 'react';
+=======
+import React, { Component } from 'react';
+import TablaTutoriasPendientes from './TablaTutoriasPendientes';
+>>>>>>> a35cf75 (TablaDeTutoriasPendientes)
 
 const Inicioview = () => {
         return (
             <div>
+<<<<<<< HEAD
                <h1> Pagina Principal </h1> 
+=======
+               <TablaTutoriasPendientes/>
+>>>>>>> a35cf75 (TablaDeTutoriasPendientes)
             </div>
         );
 }
---
+++ src/componentes/TablaTutoriasPendientes.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../css/TablaTutoriasPendientes.css';
+
+const TablaTutoriasPendientes = () => {
+
+    
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Materia</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Estado</th>
+                    <th>Fecha</th>
+                    <th>Duracion</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    );
+};
+
+export default TablaTutoriasPendientes;
