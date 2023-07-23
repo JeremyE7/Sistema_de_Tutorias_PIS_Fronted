2bfe5703f8e7f35142e63d122485e1f0d9902a42
SpecterNight <jainerpinta25@gmail.com>
2023-07-22 23:56:35 -0500

    Actualizar estilo a Paginacion

--- src/componentes/PaginacionTabla.jsx
+++ src/componentes/PaginacionTabla.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import "../css/Bootstrap.css";
+import "../css/PaginacionTabla.css";
 
-const PaginacionTabla = ({ totalItems, itemsPorPagina, paginaActual, setPagina}) => {
+const PaginacionTabla = ({ totalItems, itemsPorPagina, paginaActual, setPagina }) => {
     let paginas = [Math.ceil(totalItems / itemsPorPagina) + 1];
 
-    function paginaSiguiente(){
-        if(paginaActual!==paginas.length){
-            setPagina(paginaActual+1)
+    function paginaSiguiente() {
+        if (paginaActual !== paginas.length) {
+            setPagina(paginaActual + 1)
         }
     }
 
-    function paginaAnterior(){
-        if(paginaActual>1){
-            setPagina(paginaActual-1)
+    function paginaAnterior() {
+        if (paginaActual > 1) {
+            setPagina(paginaActual - 1)
         }
     }
     return (
         <>
-            <nav aria-label="..."> 
+            <nav aria-label="..." style={{marginTop:"10px"}}>
                 <ul className="pagination justify-content-center">
                     <li className='page-item'>
                         <a href="/#" className='page-link' onClick={paginaAnterior}>Anterior</a>
                     </li>
-                    {paginas.map((pagina, i)=>(
-                        <li className="page-item" key={i}>
-                            <a href="/#" className="page-item" onClick={()=>setPagina(pagina)}>{pagina}</a>
+                    {paginas.map((pagina, i) => (
+                        <li className={`page-item ${paginaActual === paginaActual ? 'active':''}`} style={{background:'black'}}key={i}>
+                            <a href="/#" className={"page-link"} onClick={() => setPagina(pagina)}>{pagina}</a>
                         </li>
                     ))}
                     <li className='page-item'>
