d4d3b92565efd065e37a5aa5350379c470728b74
SpecterNight <jainerpinta25@gmail.com>
2023-07-23 22:26:21 -0500

    Separar funcionalidad por rol

--- src/componentes/Inicioview.jsx
+++ src/componentes/Inicioview.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TablaTutoriasPendientes from './TablaTutoriasPendientes';
 import TablaHistorialTutorias from './TablaHistorialTutorias';
+import { Link } from 'react-router-dom';
+import { obtenerRolCuenta } from '../hooks/Conexionsw';
+import { set } from 'react-hook-form';
 
 const Inicioview = () => {
-        return (
+    const [esDocente, setDocente] = useState(false)
+
+    const asignarPermiso = (rol) =>{
+        if(rol === 'DOCENTE'){
+            setDocente(true)
+        }
+    }
+    useEffect(()=>{
+        const external = localStorage.getItem('ExternalCuenta')
+        const rol = obtenerRolCuenta(external).then(result => asignarPermiso(result.nombre))
+    })
+
+    return (
+        <div>
+            {esDocente === true && <div style={{margin:'20px'}}>
+                <Link to="/solicitar">
+                    <button style={{backgroundColor:'#23394d',color:'white', fontFamily:'sans-serif', fontWeight:'bold', borderColor:'#23394d', borderRadius:'4px'}}>Crear nueva tutoria</button>
+                </Link>
+            </div>}
             <div>
-                <TablaTutoriasPendientes/>
-                <TablaHistorialTutorias/>
+                <TablaTutoriasPendientes />
+                <TablaHistorialTutorias />
             </div>
-        );
+        </div>
+    );
 }
 
 export default Inicioview;
--- src/hooks/Conexionsw.js
+++ src/hooks/Conexionsw.js
@@ -122,6 +122,22 @@ export const tutoriasPendientes = async () => {
   }
 }
 
+export const obtenerRolCuenta = async(external_id)=>{
+  try {
+    const response = await fetch(URL+'/cuenta/'+external_id,{
+      method:'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': ObtenerSession()
+      }, 
+    })
+    const responseData = await response.json()
+    return responseData.data.rol
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const obtenerTutorias = async (rol,external_id) =>{
   try{
     const response = await fetch(URL + '/tutorias/'+rol+"/"+external_id,{
